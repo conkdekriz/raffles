15d05548ce26d244bd3398d64543699b7fc5fa7e
cristianfuentealba <cristianfuentealbaa@gmail.com>
2021-03-16 13:54:31 -0300

    add some changes´

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,4 +1,5 @@
 class PricesController < ApplicationController 
+before_action :set_prices, only: %i[edit update]
 layout 'rifa'
   def index
     @prices = Price.all
@@ -11,10 +12,24 @@ layout 'rifa'
   end
   
   def new
-
   end
 
+  def edit;
+  end
+
+  def update
+    Price.find(@price.id).update(price_params(params))
+    redirect_to all_prices_path
+  end
+
+  def all
+    @prices = Price.all
+  end
   private
+
+  def set_prices
+    @price = Price.find(params[:id])
+  end
 
   def price_params(params)
     params.require(:price).permit(
