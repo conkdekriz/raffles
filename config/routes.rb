b7523b4e1c9bd7a7b2b0e9b063f6d294310fc6fa
cristian fuentealba <cristianfuentealbaa@gmail.com>
2022-05-12 11:54:48 -0400

    addsomechanges6

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "prices#index"
-
+  get "raffle/:id/payed" => "foo#payed"
   resources :prices
     get 'all', to: 'prices#all', as: 'all_prices'
   resources :raffles do
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
       get :unpaid
       get :gracias
       get :paid
+
     end
     member do 
       post :response_paid
